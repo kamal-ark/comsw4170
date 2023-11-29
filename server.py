c40e1c76c647ee06b4483f206afcca43321fe612
Kamalakannan Amaravathy Rajarajan <kannanjunoh@gmail.com>
2023-11-29 08:06:24 -0500

    Added a Flask server with endpoints to display static webpages. One webpage will be updated in the next commit

---
+++ server.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask import render_template
+# Set template folder to folder with HTML files
+app = Flask(__name__, template_folder='')
+
+@app.route('/')
+def home():
+	return homepage()
+
+@app.route('/home')
+def homepage():
+	return render_template('homepage.html')
+
+@app.route('/review')
+def reviewpage():
+	return render_template('single_room_review.html')
+
+@app.route('/room')
+def roompage():
+	return render_template('room_info_page.html')
+
+@app.route('/addreview')
+def addreviewpage():
+	return render_template('review_add.html')
+
+@app.route('/allreviews')
+def allreviews():
+	return render_template('room_info_page.html')
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
